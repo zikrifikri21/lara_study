c3fb64e51720754e271fa4bd22c297aac7a63d4e
Ilham J.E <ilham5566@gmail.com>
2023-01-17 17:06:28 +0800

    Login dan Template admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.layout.master')
+    @section('header')
+
+    @endsection
+    @section('body')
+    <div class="container-fluid">
+        <div class="card">
+            <img class="card-img-top" src="holder.js/100x180/" alt="">
+            <div class="card-body">
+                <h4 class="card-title">Selamat Datang di halaman admin</h4>
+
+            </div>
+        </div>    
+    </div><!-- /.container-fluid -->
+    @endsection
+    @section('footer')
+    
+    @endsection
+@show
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->group(function () {
+    Route::get('dashboard', 'HomeController@index');
+});
